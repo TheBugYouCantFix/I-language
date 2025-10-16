2919fcdc5325ec628fbf63285a1fa51b591e3172
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2025-10-16 18:23:34 +0300

    rm some comments

--- src/main/scala/parser/structures/Declaration.scala
+++ src/main/scala/parser/structures/Declaration.scala
@@ -1,62 +1,35 @@
 package parser.structures
 
-/**
- * Simple declaration (variable or type declaration)
- */
 trait SimpleDeclaration extends Declaration
 
-/**
- * Variable declaration
- */
 case class VariableDeclaration(
   identifier: String,
   typeAnnotation: Option[Type],
   initializer: Option[Expression]
 ) extends SimpleDeclaration
 
-/**
- * Type declaration
- */
 case class TypeDeclaration(
   identifier: String,
   typeDefinition: Type
 ) extends SimpleDeclaration
 
-/**
- * Routine declaration
- */
 case class RoutineDeclaration(
   header: RoutineHeader,
   body: Option[RoutineBody]
 ) extends Declaration
 
-/**
- * Routine header containing name, parameters, and return type
- */
 case class RoutineHeader(
   identifier: String,
   parameters: List[ParameterDeclaration],
   returnType: Option[Type]
 )
 
-/**
- * Parameter declaration for routines
- */
 case class ParameterDeclaration(
   identifier: String,
   parameterType: Type
 )
 
-/**
- * Routine body containing local declarations and statements
- */
 sealed trait RoutineBody
-
 case class JustRoutineBody(body: Body) extends RoutineBody
-
 case class RoutineBodyExpression(expression: Expression) extends RoutineBody
-
-/**
- * Statement declaration (for top-level statements)
- */
 case class StatementDeclaration(statements: List[Statement]) extends Declaration
